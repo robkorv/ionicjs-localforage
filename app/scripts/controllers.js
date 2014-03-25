9e7651bf63bba6d3bc3da1faae9dc5c6e33d142b
Robbert Korving <r.korving@gmail.com>
2014-03-25 22:37:59 +0100

    code convention pass

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,13 +4,17 @@ angular.module('IonicjsLocalforage.controllers', [])
 
 
 // A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function ($scope, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pets = PetService.all();
-})
+.controller('PetIndexCtrl', ['$scope', 'PetService',
+    function($scope, PetService) {
+        // "Pets" is a service returning mock data (services.js)
+        $scope.pets = PetService.all();
+    }
+])
 
 // A simple controller that shows a tapped item's data
-.controller('PetDetailCtrl', function ($scope, $stateParams, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pet = PetService.get($stateParams.petId);
-});
+.controller('PetDetailCtrl', ['$scope', '$stateParams', 'PetService',
+    function($scope, $stateParams, PetService) {
+        // "Pets" is a service returning mock data (services.js)
+        $scope.pet = PetService.get($stateParams.petId);
+    }
+]);
